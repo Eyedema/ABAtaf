b3c88fb87983fc316c96a05934adc14af2980207
Ubaldo Puocci <u.puocci@gmail.com>
2019-10-09 12:36:51 +0200

    functioning prototype

--- src/app/components/forms/forms.component.ts
+++ src/app/components/forms/forms.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { AtafserviceService } from '../../services/atafservice.service';
 import { BridgeService } from '../../services/bridge.service';
+import {formatDate, registerLocaleData } from '@angular/common';
+import localeIt from '@angular/common/locales/it';
 
 import { Stop } from '../../stop'
 import { ResultDetailComponent } from '../result-detail/result-detail.component'
@@ -17,6 +19,8 @@ export class FormsComponent implements OnInit {
   name : string;
   resultCode: any;
   resultName: any;
+  today = new Date();
+  date = new Date(this.today.getFullYear(),this.today.getMonth(),this.today.getDate(),2);
 
   constructor(private service : AtafserviceService, private bridge: BridgeService, private resultDetail: ResultDetailComponent, private stopDetail: StopDetailComponent) { }
 
@@ -32,11 +36,9 @@ export class FormsComponent implements OnInit {
     this.service.getData(endpoint)
       .subscribe((data: any[])=> {
         if(!flag){          
-          this.resultCode = data;
-          this.bridge.setResultCode(data);
+          this.resultCode = this.setCorrectDate(data);
         } else{
           this.resultName = data;
-          this.bridge.setResultName(data);
         }
       });
   }
@@ -47,9 +49,21 @@ export class FormsComponent implements OnInit {
 
   searchStops(){
     if(this.name!=''){
-      console.log('chiamato searchStops()');
       this.pullResults('http://www.temporealeataf.it/Mixer/Rest/PublicTransportService.svc/search?urLat=44&urLon=12&llLat=43&llLon=10&st='+this.name, true);
     }
+  }
+
+  buildDate(atafNumber: any){
+    return formatDate(new Date(Number(this.date.getTime())+Number(atafNumber)),'dd/MM HH:mm','en-EN');
+  }
+
+  public setCorrectDate(data: any){
+    if(data != undefined){
+      data.forEach(element => {
+        element['d']=this.buildDate(element['d'])
+      });
+    }
+    return data;
   }
   
 }
--- src/app/components/result-detail/result-detail.component.ts
+++ src/app/components/result-detail/result-detail.component.ts
@@ -1,10 +1,6 @@
 import { Component, OnInit, Input, Injectable } from '@angular/core';
 import {formatDate, registerLocaleData } from '@angular/common';
-import localeIt from '@angular/common/locales/it';
 
-
-import { BridgeService } from '../../services/bridge.service';
-registerLocaleData(localeIt, 'it');
 
 @Injectable({
   providedIn: 'root'
@@ -18,40 +14,18 @@ registerLocaleData(localeIt, 'it');
 export class ResultDetailComponent implements OnInit {
 
 
-  constructor(private bridge: BridgeService) { }
+  constructor() { }
 
   @Input() code: any;
-  resultservice: any;
-  today = new Date();
-  date = new Date(this.today.getFullYear(),this.today.getMonth(),this.today.getDate(),2);
-  oktorender : boolean = false;
-
-  public setCorrectDate(){
-    if(this.resultservice != undefined){
-      console.log('entrato nel ciclo per cambio data');
-      this.resultservice.forEach(element => {
-        element['d']=this.buildDate(element['d'])
-      });
-      this.oktorender = true;
-    }
-  }
+  @Input() result: any;
 
   ngOnInit() {
   }
 
   ngOnChanges(){
-    this.resultservice = this.bridge.getResultCode();
-    console.log('ngOnChanges ', this.resultservice)
-    this.setCorrectDate();
   }
 
   ngAfterViewChecked(){
   }
-
-  buildDate(atafNumber: any){ 
-    console.log('chiamato buildDate()');
-    return formatDate(new Date(Number(this.date.getTime())+Number(atafNumber)),'dd/MM HH:mm', 'it');
-  }
-
   
 }
